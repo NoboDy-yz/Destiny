7de42cf97c78b843ee7f2c8811185e968f541805
NoboDa <116868921+NoboDy-yz@users.noreply.github.com>
2022-12-09 18:20:34 +0800

    Update and rename Bdcloud Vip.js to BdcloudVip.js

--- Script/Bdcloud Vip.js
+++
@@ -1,4 +0,0 @@
-# > 百度云Vip
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://github.com/NoboDy-us/Destiny/Scripts/raw/master/bdcloud.js
-
-hostname = pan.baidu.com
---
+++ Script/BdcloudVip.js
@@ -0,0 +1,2 @@
+# > 百度云Vip
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NoboDy-yz/Destiny/main/Script/BdcloudVip.js
