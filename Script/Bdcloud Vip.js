8ad9d3246808cd0abba02028c5239cd5b81f40b8
NoboDa <116868921+NoboDy-yz@users.noreply.github.com>
2022-12-09 18:19:20 +0800

    Update and rename Script/VIP/bd.conf to Script/Bdcloud Vip.js

---
+++ Script/Bdcloud Vip.js
@@ -0,0 +1,4 @@
+# > 百度云Vip
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://github.com/NoboDy-us/Destiny/Scripts/raw/master/bdcloud.js
+
+hostname = pan.baidu.com
