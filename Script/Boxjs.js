3ef9c46dd5bbafa9296ce07790b533b3376a38dc
NoboDa <116868921+NoboDy-yz@users.noreply.github.com>
2022-12-09 14:10:15 +0800

    Update Boxjs.js

--- Script/Boxjs.js
+++ Script/Boxjs.js
@@ -1,1 +1,1 @@
-https?:\/\/boxjs\.(com|net) url script-analyze-echo-response https://github.com/NoboDy-yz/Destiny/main/Script/Boxjs.js
+https?:\/\/boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/NoboDy-yz/Destiny/main/Script/Boxjs.js
